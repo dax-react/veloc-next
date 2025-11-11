6b3e789768aa7cb407cc7878c402e5e44dbed498
dax-react <dxpatel.dev@gmail.com>
2025-11-11 14:49:22 +0530

    build fix

---
+++ app/dedicated_developers_forms/page.js
@@ -0,0 +1,8 @@
+// app/dedicated_developers_forms/page.tsx
+export default function DedicatedDevelopersFormPage() {
+    return (
+        <div>
+            <h1>Dedicated Developers Form</h1>
+        </div>
+    );
+}
